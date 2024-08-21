84e41d6e95e8a2d2e827891a2a727e81ab9ca02d
hanuman2922 <hanuman2922@gmail.com>
2024-08-21 17:45:27 +0530

    commit1

---
+++ api-gateway1/src/main/java/com/Main.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(Main.class, args);
+
+	}
+
+}
